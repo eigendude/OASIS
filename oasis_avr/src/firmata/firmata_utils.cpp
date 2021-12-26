1e905039d5ac4af364866e4d23ab9a3a671ea49a
Garrett Brown <eigendebugger@gmail.com>
2021-12-26 13:17:34 -0800

    avr: Fix copyright headers

--- oasis_avr/src/firmata/firmata_utils.cpp
+++ oasis_avr/src/firmata/firmata_utils.cpp
@@ -2,12 +2,12 @@
  *  Copyright (C) 2021 Garrett Brown
  *  This file is part of OASIS - https://github.com/eigendude/OASIS
  *
- *  This file is derived from the FirmataExpress project and the AGPL-3. License
+ *  This file is derived from FirmataExpress under the AGPL 3.0 License
  *  Copyright (C) 2006-2008 Hans-Christoph Steiner. All rights reserved.
  *  Copyright (C) 2009-2017 Jeff Hoefs. All rights reserved.
  *  Copyright (C) 2018-2019 Alan Yorinks. All Rights Reserved.
  *
- *  SPDX-License-Identifier: Apache-2.0 AND AGPL-3
+ *  SPDX-License-Identifier: Apache-2.0 AND AGPL-3.0
  *  See DOCS/LICENSING.md for more information.
  */
 
