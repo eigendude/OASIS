1eb5d3935a6cc20f714b3e2d652a59302561e6bc
Garrett Brown <eigendebugger@gmail.com>
2022-03-25 17:57:47 -0700

    tooling: Add build_all.sh script

---
+++ oasis_tooling/scripts/build_all.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+################################################################################
+#
+#  Copyright (C) 2021 Garrett Brown
+#  This file is part of OASIS - https://github.com/eigendude/OASIS
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See the file LICENSE.txt for more information.
+#
+################################################################################
+
+# Enable strict mode
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#
+# Environment paths and configuration
+#
+
+# Get the absolute path to this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+# Install all system dependencies first
+#
+
+"${SCRIPT_DIR}/depinstall_cmake.sh"
+"${SCRIPT_DIR}/depinstall_ros1_desktop.sh"
+"${SCRIPT_DIR}/depinstall_ros2_desktop.sh"
+
+#
+# Build CMake
+#
+
+"${SCRIPT_DIR}/build_cmake.sh"
+
+#
+# Build ROS 1
+#
+
+"${SCRIPT_DIR}/build_ros1_desktop.sh"
+
+#
+# Build ROS 2
+#
+
+"${SCRIPT_DIR}/build_ros2_desktop.sh"
+
+#
+# Build OASIS dependencies
+#
+
+"${SCRIPT_DIR}/depinstall_oasis_deps.sh"
+"${SCRIPT_DIR}/build_oasis_deps.sh"
+
+#
+# Build OASIS
+#
+
+"${SCRIPT_DIR}/depinstall_oasis.sh"
+"${SCRIPT_DIR}/build_oasis.sh"
