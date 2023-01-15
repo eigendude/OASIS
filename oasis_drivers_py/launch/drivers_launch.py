38a03b2623a3bcb8ec55fc5d2fcbecd644c3a736
Garrett Brown <eigendebugger@gmail.com>
2023-01-15 12:58:56 -0800

    drivers_py: Enable video on lenovo

--- oasis_drivers_py/launch/drivers_launch.py
+++ oasis_drivers_py/launch/drivers_launch.py
@@ -61,7 +61,7 @@ elif HOSTNAME == "inspiron":
     ENABLE_DISPLAY = True
 elif HOSTNAME == "lenovo":
     ENABLE_DISPLAY = True
-    ENABLE_VIDEO = False
+    ENABLE_VIDEO = True
 elif HOSTNAME == "netbook":
     ENABLE_DISPLAY = True
     ENABLE_VIDEO = False
