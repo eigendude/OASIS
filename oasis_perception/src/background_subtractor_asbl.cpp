3e7f1b94e83e0fa0e5e4a7c1ff367163e79fa6c6
Garrett Brown <eigendebugger@gmail.com>
2023-01-15 13:02:47 -0800

    [temp] Configure lenovo

--- oasis_perception/src/background_subtractor_asbl.cpp
+++ oasis_perception/src/background_subtractor_asbl.cpp
@@ -23,9 +23,11 @@ constexpr const char* BGS_ASBL_NODE_NAME = "background_substractor_asbl";
 
 // TODO: Hardware configuration
 constexpr const char* VIDEO_MACHINE_KINECT2 = "kinect2";
+constexpr const char* VIDEO_MACHINE_LENOVO = "lenovo";
 
 // Subscribed topics
 constexpr const char* IMAGE_TOPIC_KINECT2 = "hd/image_color";
+constexpr const char* IMAGE_TOPIC_LENOVO = "image_raw";
 
 // Published topics
 constexpr const char* FOREGROUND_TOPIC = "foreground";
@@ -44,13 +46,12 @@ int main(int argc, char* argv[])
       std::make_shared<rclcpp::Node>(BGS_ASBL_NODE_NAME + appendHostname);
 
   // Create topics
-  const std::string kinectTopicBase =
-      std::string("/") + ROS_NAMESPACE + "/" + VIDEO_MACHINE_KINECT2 + "/";
+  const std::string topicBase = std::string("/") + ROS_NAMESPACE + "/" + VIDEO_MACHINE_LENOVO + "/";
 
   {
-    OASIS::IMAGE::BackgroundModelerASBL backgroundModelerKinect2(
-        node, kinectTopicBase + IMAGE_TOPIC_KINECT2, kinectTopicBase + FOREGROUND_TOPIC,
-        kinectTopicBase + SUBTRACTED_TOPIC);
+    OASIS::IMAGE::BackgroundModelerASBL backgroundModeler(node, topicBase + IMAGE_TOPIC_LENOVO,
+                                                          topicBase + FOREGROUND_TOPIC,
+                                                          topicBase + SUBTRACTED_TOPIC);
 
     rclcpp::spin(node);
   }
