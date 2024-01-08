039f85e872b3ef2bdc1210abfc25e1acf78fecea
Garrett Brown <eigendebugger@gmail.com>
2024-01-07 17:22:46 -0800

    tooling: Remove used variable in env_rpi.sh

--- oasis_tooling/scripts/env_rpi.sh
+++ oasis_tooling/scripts/env_rpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ################################################################################
 #
-#  Copyright (C) 2023 Garrett Brown
+#  Copyright (C) 2023-2024 Garrett Brown
 #  This file is part of OASIS - https://github.com/eigendude/OASIS
 #
 #  SPDX-License-Identifier: Apache-2.0
@@ -17,10 +17,6 @@ set -o nounset
 #
 # Disable interactive mode for apt
 #
-
-export NEEDRESTART_MODE="a"
-
-#
 # In a normal install, this variable as no effect, because sudo doesn't keep
 # environment variables by default. In order to pass this variable to sudo,
 # add the following to your sudoers file (i.e. sudo visudo):
@@ -28,10 +24,4 @@ export NEEDRESTART_MODE="a"
 #   Defaults        env_keep += "NEEDRESTART_MODE"
 #
 
-NEEDED_LINE="Defaults        env_keep += \"NEEDRESTART_MODE\""
-
-# Check if the sudoers file has been modified with the suggested line above
-if ! sudo grep -q "NEEDRESTART_MODE" "/etc/sudoers"; then
-  echo "Please add the following line to your sudoers file (i.e. sudo visudo):"
-  echo "  ${NEEDED_LINE}"
-fi
+export NEEDRESTART_MODE="a"
