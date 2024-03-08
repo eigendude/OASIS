c237ab7f8f32951375abd4e021efc3b0f43bef80
Garrett Brown <eigendebugger@gmail.com>
2024-03-08 23:44:49 +0000

    visualization: Add CI

---
+++ oasis_visualization/.eslintrc.js
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (C) 2024 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    imports: "readonly",
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
