b5d4035134b6ad29a57a1dc0182263f19859bc76
Garrett Brown <eigendebugger@gmail.com>
2024-06-24 17:25:56 -0700

    visualization: Update no-overview-45 metadata

---
+++ oasis_visualization/extensions/no-overview-45/README.md
@@ -0,0 +1,7 @@
+## No overview at start-up (Gnome 45+)
+
+![No overview at start-up icon](icon.png)
+
+No overview at start-up for Gnome 45+. Nothing more.
+
+Adapted for Gnome 45+ from the original extension by [fthx](https://extensions.gnome.org/accounts/profile/fthx) for my OASIS smarthome OS (https://github.com/eigendude/OASIS).
--- oasis_visualization/extensions/no-overview-45/extension.js
+++ oasis_visualization/extensions/no-overview-45/extension.js
@@ -3,7 +3,8 @@
  *  This file is part of OASIS - https://github.com/eigendude/OASIS
  *
  *  This file is derived from no-overview under the GPL v3 license.
- *  https://github.com/fthx/no-overview
+ *  Copyright (C) fthx
+ *  https://extensions.gnome.org/extension/4099/no-overview/
  *
  *  SPDX-License-Identifier: Apache-2.0 AND GPL-3.0-or-later
  *  See the file LICENSE.txt for more information.
@@ -13,19 +14,43 @@ import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class NoOverviewExtension extends Extension {
+  /**
+   * @brief Construct an extension object
+   *
+   * Called once when extension is loaded, not enabled.
+   *
+   * @param {ExtensionMeta} metadata - An extension meta object
+   */
+  constructor(metadata) {
+    super(metadata);
+
+    console.debug(`Constructing ${this.metadata.name}`);
+  }
+
+  /**
+   * @brief Function called when extension is enabled
+   */
   enable() {
+    // If the layout manager is not starting up, do nothing
     if (!Main.layoutManager._startingUp) {
       return;
     }
 
+    // Connect to the 'startup-complete' signal to hide the overview
+    // when the startup process is complete
     Main.layoutManager.connectObject(
       "startup-complete",
-      () => Main.overview.hide(),
+      () => Main.overview.hide(), // Hide the overview
       this,
     );
   }
 
+  /**
+   * @brief Function called when extension is uninstalled, disabled, or when the
+   *        screen locks
+   */
   disable() {
+    // Disconnect all signals connected to this object
     Main.layoutManager.disconnectObject(this);
   }
 }
