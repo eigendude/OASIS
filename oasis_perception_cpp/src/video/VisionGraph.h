1246a9c686be89cca84306811e319d18479a3b19
Garrett Brown <garrett.brown@aclima.earth>
2025-09-23 21:18:00 -0700

    perception_cpp: Add Optical Flow node

---
+++ oasis_perception_cpp/src/video/VisionGraph.h
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2020-2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+
+#pragma once
+
+#include <stdint.h>
+#include <vector>
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/gapi/gcompiled.hpp>
+
+namespace OASIS
+{
+namespace VIDEO
+{
+class VisionGraph
+{
+public:
+  VisionGraph() = default;
+  ~VisionGraph();
+
+  void Compile(unsigned int width,
+               unsigned int height,
+               const cv::Mat& currentFrame,
+               const cv::Mat& currentGrayscale,
+               const cv::Mat& previousGrayscale);
+
+  void ApplyGrayscale(
+      // Input
+      const cv::Mat& currentFrame,
+      // Output
+      cv::Mat& currentGrayscale);
+
+  void FindFeatures(
+      // Input
+      const cv::Mat& currentGrayscale,
+      unsigned int maxFeatures,
+      double minDistance,
+      // Output
+      std::vector<cv::Point2f>& currentPoints);
+
+  void CalcOpticalFlow(
+      // Input
+      const cv::Mat& previousGrayscale,
+      const cv::Mat& currentGrayscale,
+      const std::vector<cv::Point2f>& previousPoints,
+      const std::vector<std::vector<cv::Point2f>>& pointHistory,
+      // Output
+      std::vector<cv::Point2f>& currentPoints,
+      std::vector<uchar>& status,
+      std::vector<float>& errors);
+
+private:
+  cv::GCompiled m_applyGrayscale;
+  cv::GCompiled m_findFeatures;
+  cv::GCompiled m_calcOpticalFlow;
+};
+} // namespace VIDEO
+} // namespace OASIS
