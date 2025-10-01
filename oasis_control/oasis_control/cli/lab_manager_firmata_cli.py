232c12d1850bcd536f1757d86084836c7ad9c3ff
Garrett Brown <eigendebugger@gmail.com>
2025-10-01 17:36:51 +0000

    control: Add lab_manager_firmata node

---
+++ oasis_control/oasis_control/cli/lab_manager_firmata_cli.py
@@ -0,0 +1,31 @@
+################################################################################
+#
+#  Copyright (C) 2022-2025 Garrett Brown
+#  This file is part of OASIS - https://github.com/eigendude/OASIS
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See DOCS/LICENSING.md for more information.
+#
+################################################################################
+
+import rclpy
+
+from oasis_control.nodes.lab_manager_firmata_node import LabManagerNode
+
+
+################################################################################
+# ROS entry point
+################################################################################
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+
+    node = LabManagerNode()
+    if node.initialize():
+        rclpy.spin(node)
+
+        # Destroy the node explicitly. Problems can occur when the garbage
+        # collector automatically destroys the node object after ROS has
+        # shut down.
+        node.destroy_node()
--- oasis_control/setup.py
+++ oasis_control/setup.py
@@ -79,6 +79,7 @@ setuptools.setup(
             "conductor_manager_telemetrix = oasis_control.cli.conductor_manager_telemetrix_cli:main",
             "engine_manager = oasis_control.cli.engine_manager_cli:main",
             "home_manager = oasis_control.cli.home_manager_cli:main",
+            "lab_manager_firmata = oasis_control.cli.lab_manager_firmata_cli:main",
             "lab_manager_telemetrix = oasis_control.cli.lab_manager_telemetrix_cli:main",
             "leonardo_manager = oasis_control.cli.leonardo_manager_cli:main",
         ],
