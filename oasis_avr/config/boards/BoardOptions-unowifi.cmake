56ed47f662bf475604c367359662cfd0abe2126d
Garrett Brown <eigendebugger@gmail.com>
2025-10-02 01:12:21 +0000

    avr/tooling: Add support for Arduino Uni Wifi Rev2

---
+++ oasis_avr/config/boards/BoardOptions-unowifi.cmake
@@ -0,0 +1,23 @@
+################################################################################
+#
+#  Copyright (C) 2025 Garrett Brown
+#  This file is part of OASIS - https://github.com/eigendude/OASIS
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See the file LICENSE.txt for more information.
+#
+################################################################################
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Arduino boards.
+# Platform: Arduino AVR Boards
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+set(ARDUINO_BOARD "Arduino Uno WiFi Rev2 [megaavr.uno2018]") # Arduino Uno WiFi Rev2
+
+#******************************************************************************
+# Arduino programmers.
+# Platform: Arduino AVR Boards
+#******************************************************************************
+
+# No ARDUINO_PROGRAMMER is set to avoid confusion - megaavr will still select UPDI
