67e243f7e2fbef82e49fecf68fee775d8c877d48
Garrett Brown <eigendebugger@gmail.com>
2025-10-02 01:12:21 +0000

    avr: Change air quality lab to Arduino Uni Wifi Rev2

---
+++ oasis_avr/config/BoardOptions-unowifi.cmake
@@ -0,0 +1,21 @@
+# Copyright (c) 2020 Arduino CMake Toolchain
+
+###############################################################################
+# This is an automatically generated template file for board options.
+# You may edit it to comment/uncomment selected board and board options.
+# However do not change the structure of this template, which is fixed and
+# any change to the structure gets overwritten.
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Arduino boards.
+# Platform: Arduino AVR Boards
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+set(ARDUINO_BOARD "Arduino Uno WiFi Rev2 [megaavr.uno2018]") # Arduino Uno WiFi Rev2
+
+#******************************************************************************
+# Arduino programmers.
+# Platform: Arduino AVR Boards
+#******************************************************************************
+
+set(ARDUINO_PROGRAMMER "AVR ISP [avr.avrisp]") # AVR ISP
