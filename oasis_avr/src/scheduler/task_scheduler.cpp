eec98188524c2ed1931df30a73f485e7e3fdbf04
Garrett Brown <garrett.brown@aclima.io>
2025-10-01 20:34:34 -0700

    avr: Replace Scheduler library with TaskScheduler

---
+++ oasis_avr/src/scheduler/task_scheduler.cpp
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See DOCS/LICENSING.md for more information.
+ */
+
+#include "task_scheduler.hpp"
+
+#include <TScheduler.hpp>
+
+namespace OASIS
+{
+namespace
+{
+TsScheduler g_scheduler;
+bool g_initialized{false};
+} // namespace
+
+void InitializeTaskScheduler()
+{
+  if (!g_initialized)
+  {
+    g_scheduler.init();
+    g_initialized = true;
+  }
+}
+
+TsScheduler& GetTaskScheduler()
+{
+  InitializeTaskScheduler();
+  return g_scheduler;
+}
+
+void RunTaskScheduler()
+{
+  InitializeTaskScheduler();
+  g_scheduler.execute();
+}
+
+} // namespace OASIS
