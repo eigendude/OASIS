489705e7ab875084be5ed6683066bd5b8b96811d
Ubuntu <eigendebugger@gmail.com>
2025-10-03 19:58:26 -0700

    tooling: Combine apt commands

--- oasis_tooling/scripts/depinstall_kodi.sh
+++ oasis_tooling/scripts/depinstall_kodi.sh
@@ -29,113 +29,123 @@ source "${SCRIPT_DIR}/env_kodi.sh"
 #
 
 if [[ "${OSTYPE}" != "darwin"* ]]; then
-  sudo apt install -y \
-    autoconf \
-    automake \
-    autopoint \
-    autotools-dev \
-    ccache \
-    cmake \
-    curl \
-    debhelper \
-    default-jre \
-    doxygen \
-    gawk \
-    gcc \
-    gdc \
-    gettext \
-    gperf \
-    libasound2-dev \
-    libass-dev \
-    libavahi-client-dev \
-    libavahi-common-dev \
-    libbluetooth-dev \
-    libbluray-dev \
-    libbz2-dev \
-    libcdio-dev \
-    libcec-dev \
-    libcrossguid-dev \
-    libcurl4-openssl-dev \
-    libcwiid-dev \
-    libdbus-1-dev \
-    libdisplay-info-dev \
-    libdrm-dev \
-    libegl1-mesa-dev \
-    libenca-dev \
-    libexiv2-dev \
-    libflac-dev \
-    libflatbuffers-dev \
-    libfmt-dev \
-    libfontconfig-dev \
-    libfreetype6-dev \
-    libfribidi-dev \
-    libfstrcmp-dev \
-    libgcrypt-dev \
-    libgif-dev \
-    libgl1-mesa-dev \
-    libgles2-mesa-dev \
-    libglu1-mesa-dev \
-    libgnutls28-dev \
-    libgpg-error-dev \
-    libgtest-dev \
-    libinput-dev \
-    libiso9660-dev \
-    libjpeg-dev \
-    liblcms2-dev \
-    liblirc-dev \
-    libltdl-dev \
-    liblzo2-dev \
-    libmicrohttpd-dev \
-    libmysqlclient-dev \
-    libnfs-dev \
-    libogg-dev \
-    libp8-platform-dev \
-    libpcre2-dev \
-    libplist-dev \
-    libpng-dev \
-    libpulse-dev \
-    libshairplay-dev \
-    libsmbclient-dev \
-    libspdlog-dev \
-    libsqlite3-dev \
-    libssl-dev \
-    libtag1-dev \
-    libtiff5-dev \
-    libtinyxml-dev \
-    libtinyxml2-dev \
-    libtool \
-    libudev-dev \
-    libunistring-dev \
-    libva-dev \
-    libvdpau-dev \
-    libvorbis-dev \
-    libxmu-dev \
-    libxrandr-dev \
-    libxslt1-dev \
-    libxt-dev \
-    lsb-release \
-    meson \
-    nasm \
-    ninja-build \
-    nlohmann-json3-dev \
-    python3-dev \
-    python3-pil \
-    python3-pip \
-    swig \
-    unzip \
-    uuid-dev \
-    zip \
-    zlib1g-dev \
+  # Refresh package metadata
+  sudo apt update
+
+  # Packages to install via apt
+  APT_PACKAGES=(
+    # Kodi build dependencies
+    autoconf
+    automake
+    autopoint
+    autotools-dev
+    ccache
+    cmake
+    curl
+    debhelper
+    default-jre
+    doxygen
+    gawk
+    gcc
+    gdc
+    gettext
+    gperf
+    libasound2-dev
+    libass-dev
+    libavahi-client-dev
+    libavahi-common-dev
+    libbluetooth-dev
+    libbluray-dev
+    libbz2-dev
+    libcdio-dev
+    libcec-dev
+    libcrossguid-dev
+    libcurl4-openssl-dev
+    libcwiid-dev
+    libdbus-1-dev
+    libdisplay-info-dev
+    libdrm-dev
+    libegl1-mesa-dev
+    libenca-dev
+    libexiv2-dev
+    libflac-dev
+    libflatbuffers-dev
+    libfmt-dev
+    libfontconfig-dev
+    libfreetype6-dev
+    libfribidi-dev
+    libfstrcmp-dev
+    libgcrypt-dev
+    libgif-dev
+    libgl1-mesa-dev
+    libgles2-mesa-dev
+    libglu1-mesa-dev
+    libgnutls28-dev
+    libgpg-error-dev
+    libgtest-dev
+    libinput-dev
+    libiso9660-dev
+    libjpeg-dev
+    liblcms2-dev
+    liblirc-dev
+    libltdl-dev
+    liblzo2-dev
+    libmicrohttpd-dev
+    libmysqlclient-dev
+    libnfs-dev
+    libogg-dev
+    libp8-platform-dev
+    libpcre2-dev
+    libplist-dev
+    libpng-dev
+    libpulse-dev
+    libshairplay-dev
+    libsmbclient-dev
+    libspdlog-dev
+    libsqlite3-dev
+    libssl-dev
+    libtag1-dev
+    libtiff5-dev
+    libtinyxml-dev
+    libtinyxml2-dev
+    libtool
+    libudev-dev
+    libunistring-dev
+    libva-dev
+    libvdpau-dev
+    libvorbis-dev
+    libxmu-dev
+    libxrandr-dev
+    libxslt1-dev
+    libxt-dev
+    lsb-release
+    meson
+    nasm
+    ninja-build
+    nlohmann-json3-dev
+    python3-dev
+    python3-pil
+    python3-pip
+    swig
+    unzip
+    uuid-dev
+    zip
+    zlib1g-dev
+
+    # Wayland dependencies
+    libglew-dev
+    libwayland-dev
+    libxkbcommon-dev
+    wayland-protocols
+    waylandpp-dev
+  )
 
   if [ "${ENABLE_LLD}" = "ON" ]; then
-    sudo apt install -y lld
+    # Optional LLD linker
+    APT_PACKAGES+=(
+      lld
+    )
   fi
 
-  # Wayland dependencies
-  sudo apt install -y \
-    libglew-dev \
-    libwayland-dev \
-    libxkbcommon-dev \
-    wayland-protocols \
-    waylandpp-dev
+  sudo apt install -y --no-install-recommends "${APT_PACKAGES[@]}"
 fi
