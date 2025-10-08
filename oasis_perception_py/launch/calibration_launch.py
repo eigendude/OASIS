6b5265f7b898fe084ca572c40c2aa7ef3a9f2f38
Garrett Brown <garrett.brown@aclima.io>
2025-10-07 19:16:49 -0700

    Add camera driver mapping for calibration launch

--- oasis_perception_py/launch/calibration_launch.py
+++ oasis_perception_py/launch/calibration_launch.py
@@ -8,19 +8,26 @@
 #
 ################################################################################
 
-import socket
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from oasis_hass.utils.smarthome_config import SmarthomeConfig
 
 ################################################################################
 # System parameters
 ################################################################################
 
+CONFIG: SmarthomeConfig = SmarthomeConfig()
 
-# Get the hostname
-HOSTNAME = socket.gethostname().replace("-", "_")
+# Get the hostname and host alias
+HOSTNAME: str = CONFIG.HOSTNAME
+HOST_ID: str = CONFIG.HOST_ID
+
+# Zone configuration
+ZONE_ID: str = CONFIG.ZONE_ID
+
+# Camera driver configuration
+CAMERA_DRIVER: str = CONFIG.get_camera_driver(HOST_ID, ZONE_ID)
 
 
 ################################################################################
@@ -41,6 +48,14 @@ PYTHON_PACKAGE_NAME = "oasis_perception_py"
 def generate_launch_description() -> LaunchDescription:
     ld: LaunchDescription = LaunchDescription()
 
+    camera_driver: str = CAMERA_DRIVER or "v4l2_camera"
+    if camera_driver == "v4l2_camera":
+        camera_node: str = f"v4l2_camera_{HOSTNAME}"
+    elif camera_driver == "camera_ros":
+        camera_node = f"camera_ros_{HOSTNAME}"
+    else:
+        raise ValueError(f"Unsupported camera driver: {camera_driver}")
+
     # Camera calibrator node
     calibrator_node: Node = Node(
         package=PYTHON_PACKAGE_NAME,
@@ -50,7 +65,7 @@ def generate_launch_description() -> LaunchDescription:
         output="screen",
         remappings=[
             ("calibration", f"{HOSTNAME}/calibration"),
-            ("camera/set_camera_info", f"v4l2_camera_{HOSTNAME}/set_camera_info"),
+            ("camera/set_camera_info", f"{camera_node}/set_camera_info"),
             ("image", f"{HOSTNAME}/image_raw"),
         ],
     )
