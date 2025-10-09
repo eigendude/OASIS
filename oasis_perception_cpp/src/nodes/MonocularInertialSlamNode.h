e05a95326034a154a19c7cafe5725c480902b6c5
Garrett Brown <garrett.brown@aclima.io>
2025-10-08 21:02:34 -0700

    perception_cpp: Convert monocular inertial SLAM node to component

---
+++ oasis_perception_cpp/src/nodes/MonocularInertialSlamNode.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+#pragma once
+
+#include <memory>
+
+#include <image_transport/subscriber.hpp>
+#include <oasis_msgs/msg/i2_c_imu.hpp>
+#include <rclcpp/subscription.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+namespace rclcpp
+{
+class Logger;
+class Node;
+} // namespace rclcpp
+
+namespace OASIS
+{
+namespace SLAM
+{
+class MonocularInertialSlam;
+}
+
+namespace ROS
+{
+class MonocularInertialSlamNode
+{
+public:
+  MonocularInertialSlamNode(rclcpp::Node& node);
+  ~MonocularInertialSlamNode();
+
+  bool Initialize();
+  void Deinitialize();
+
+private:
+  // ROS interface
+  void OnImage(const sensor_msgs::msg::Image::ConstSharedPtr& msg);
+  void OnImu(const oasis_msgs::msg::I2CImu::ConstSharedPtr& msg);
+
+  // Construction parameters
+  rclcpp::Node& m_node;
+
+  // ROS parameters
+  std::unique_ptr<rclcpp::Logger> m_logger;
+  std::unique_ptr<image_transport::Subscriber> m_imgSubscriber;
+  rclcpp::Subscription<oasis_msgs::msg::I2CImu>::SharedPtr m_imuSubscriber;
+
+  // Video parameters
+  std::unique_ptr<SLAM::MonocularInertialSlam> m_monocularInertialSlam;
+};
+} // namespace ROS
+} // namespace OASIS
