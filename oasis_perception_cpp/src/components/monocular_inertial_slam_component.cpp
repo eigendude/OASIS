efc22330c4d5d90c618dce27c3d50d160b963908
Garrett Brown <garrett.brown@aclima.io>
2025-10-08 21:02:34 -0700

    perception_cpp: Convert monocular inertial SLAM node to component

---
+++ oasis_perception_cpp/src/components/monocular_inertial_slam_component.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+
+#include "nodes/MonocularInertialSlamNode.h"
+#include "ros/RosComponent.h"
+
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+using namespace OASIS;
+
+namespace oasis_perception
+{
+// Default component name
+constexpr const char* ROS_COMPONENT_NAME = "monocular_inertial_slam_component";
+
+class MonocularInertialSlamComponent : public RosComponent
+{
+public:
+  MonocularInertialSlamComponent(const rclcpp::NodeOptions& options)
+    : RosComponent(ROS_COMPONENT_NAME, options)
+  {
+    m_interface = std::make_unique<ROS::MonocularInertialSlamNode>(*m_node);
+    if (!m_interface->Initialize())
+      throw std::runtime_error(std::string{"Failed to start "} + m_node->get_name());
+  }
+
+  ~MonocularInertialSlamComponent() override
+  {
+    if (m_interface)
+    {
+      m_interface->Deinitialize();
+      m_interface.reset();
+    }
+  }
+
+private:
+  std::unique_ptr<ROS::MonocularInertialSlamNode> m_interface;
+};
+} // namespace oasis_perception
+
+// Register the component with ROS 2
+RCLCPP_COMPONENTS_REGISTER_NODE(oasis_perception::MonocularInertialSlamComponent);
