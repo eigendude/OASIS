1919bd7e52ee8f7e016f2ef69169e387f505cb98
Garrett Brown <eigendebugger@gmail.com>
2025-10-11 16:21:28 -0700

    control: Increase train voltage by 4.3% to 2.18V

--- oasis_control/oasis_control/nodes/conductor_manager_telemetrix_node.py
+++ oasis_control/oasis_control/nodes/conductor_manager_telemetrix_node.py
@@ -481,7 +481,7 @@ class ConductorManagerNode(rclpy.node.Node):
             reverse: bool = throttle < 0.0
 
             # Reduce magnitude by a factor to limit top speed
-            magnitude /= 5.75
+            magnitude /= 5.5  # Max 2.18V out of 12V supply
 
             # Futures to wait on while the service is being called
             future_pwm: Optional[asyncio.Future] = None
