7c471f0330a8a9ec3e77e85cf4b5d791a15a2fff
Garrett Brown <garrett.brown@aclima.earth>
2025-10-17 18:49:57 -0700

    [temp] perception_py: Move falcon monocular SLAM to macbook_vm

--- oasis_perception_py/launch/perception_launch.py
+++ oasis_perception_py/launch/perception_launch.py
@@ -52,7 +52,7 @@ PERCEPTION_SERVER_MONOCULAR_SLAM: list[str] = []
 PERCEPTION_SERVER_POSE_LANDMARKS: list[str] = []
 
 
-if HOST_ID == "falcon":
+if HOST_ID == "macbook_vm":
     # PERCEPTION_SERVER_FLOW.extend(["falcon"])
     pass
 elif HOST_ID == "nas":
