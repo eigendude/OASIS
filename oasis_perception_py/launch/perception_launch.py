5256e95896e39c3aa60cebf498ca943f5c7ac2e9
Garrett Brown <eigendebugger@gmail.com>
2025-10-21 03:46:03 +0000

    perception_py: Enable monocular SLAM for falcon on oceanplatform

--- oasis_perception_py/launch/perception_launch.py
+++ oasis_perception_py/launch/perception_launch.py
@@ -63,7 +63,8 @@ elif HOST_ID == "nas":
     PERCEPTION_SERVER_POSE_LANDMARKS.extend(["hallway"])
 elif HOST_ID == "oceanplatform":
     # PERCEPTION_SERVER_BACKGROUND.extend(["station"])
-    PERCEPTION_SERVER_FLOW.extend(["falcon", "station"])
+    # PERCEPTION_SERVER_FLOW.extend(["falcon", "station"])
+    PERCEPTION_SERVER_MONOCULAR_SLAM.extend(["falcon"])
     # PERCEPTION_SERVER_POSE_LANDMARKS.extend(["falcon"])
 
 
