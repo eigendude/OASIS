7523f67e90b6b8ab9a6abae8434a94da63c040a6
Garrett Brown <garrett.brown@aclima.io>
2025-11-14 10:40:17 -0800

    Refactor mesh viewer point cloud rendering

---
+++ oasis_perception_cpp/src/pointcloud/MeshRenderer.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+#pragma once
+
+#include <memory>
+
+#include <rclcpp/logger.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+namespace OASIS
+{
+class MeshRenderer
+{
+public:
+  MeshRenderer(rclcpp::Logger logger,
+               double voxelLeafSize,
+               double normalSearchRadius,
+               double triangulationSearchRadius);
+  ~MeshRenderer();
+
+  [[nodiscard]] sensor_msgs::msg::Image::SharedPtr
+  Render(const sensor_msgs::msg::PointCloud2& pointCloud) const;
+
+private:
+  rclcpp::Logger m_logger;
+  double m_voxelLeafSize;
+  double m_normalSearchRadius;
+  double m_triangulationSearchRadius;
+};
+} // namespace OASIS
