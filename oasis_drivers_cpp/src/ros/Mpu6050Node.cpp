f27d7364bc0becfb44946a33d90a57995c393fa0
Garrett Brown <garrett.brown@aclima.io>
2025-12-13 09:28:17 -0800

    Add dummy MPU6050 driver node

---
+++ oasis_drivers_cpp/src/ros/Mpu6050Node.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+
+#include "Mpu6050Node.h"
+
+#include <algorithm>
+#include <chrono>
+#include <functional>
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace
+{
+constexpr const char* NODE_NAME = "mpu6050_driver";
+constexpr const char* IMU_TOPIC = "imu";
+constexpr const char* FRAME_ID = "imu_link";
+constexpr double DEFAULT_PUBLISH_RATE_HZ = 50.0;
+} // namespace
+
+using namespace OASIS::ROS;
+using namespace std::chrono_literals;
+
+Mpu6050Node::Mpu6050Node() : rclcpp::Node(NODE_NAME)
+{
+  declare_parameter("publish_rate_hz", DEFAULT_PUBLISH_RATE_HZ);
+
+  m_publisher = create_publisher<sensor_msgs::msg::Imu>(IMU_TOPIC, rclcpp::QoS(10));
+
+  const double publishRateHz = get_parameter("publish_rate_hz").as_double();
+  const double clampedRate = std::max(publishRateHz, 1.0);
+  const auto publishPeriod = std::chrono::duration<double>(1.0 / clampedRate);
+
+  m_timer = create_wall_timer(publishPeriod, std::bind(&Mpu6050Node::PublishImu, this));
+}
+
+void Mpu6050Node::PublishImu()
+{
+  auto message = sensor_msgs::msg::Imu();
+  message.header.stamp = get_clock()->now();
+  message.header.frame_id = FRAME_ID;
+
+  m_publisher->publish(message);
+}
+
