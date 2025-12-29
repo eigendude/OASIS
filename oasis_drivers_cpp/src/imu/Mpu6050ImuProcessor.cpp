aba701989c456309a4ae258d1f342de13c8c1852
Garrett Brown <garrett.brown@aclima.io>
2025-12-28 23:04:54 -0800

    Add forward axis learner and IMU yaw variants

--- oasis_drivers_cpp/src/imu/Mpu6050ImuProcessor.cpp
+++ oasis_drivers_cpp/src/imu/Mpu6050ImuProcessor.cpp
@@ -8,6 +8,8 @@
 
 #include "imu/Mpu6050ImuProcessor.h"
 
+#include "imu/ImuMath.h"
+
 #include <cmath>
 
 namespace
@@ -15,6 +17,7 @@ namespace
 constexpr double kG = 9.80665;
 constexpr std::size_t kBootTrimStationarySamples = 50;
 constexpr double kEps = 1e-9;
+constexpr double kUpAlpha = 0.1;
 } // namespace
 
 using namespace OASIS::IMU;
@@ -78,6 +81,33 @@ Mpu6050ImuProcessor::ProcessedSample Mpu6050ImuProcessor::ProcessRaw(
                        (sample.accel_raw_mps2[1] - sample.diag.bias_mps2[1]) / sample.diag.scale[1],
                        (sample.accel_raw_mps2[2] - sample.diag.bias_mps2[2]) /
                            sample.diag.scale[2]};
+
+  if (sample.diag.stationary)
+  {
+    const double accel_norm = Math::Norm(sample.accel_mps2);
+    if (accel_norm > kEps)
+    {
+      const std::array<double, 3> u_meas = Math::Scale(sample.accel_mps2, 1.0 / accel_norm);
+      if (!m_u_hat_valid)
+      {
+        m_u_hat = u_meas;
+        m_u_hat_valid = true;
+      }
+      else
+      {
+        m_u_hat = Math::Normalize(Math::Add(Math::Scale(m_u_hat, 1.0 - kUpAlpha),
+                                            Math::Scale(u_meas, kUpAlpha)));
+      }
+    }
+  }
+
+  sample.u_hat = m_u_hat;
+  sample.u_hat_valid = m_u_hat_valid;
+  sample.forward_diag =
+      m_forwardAxisLearner.Update(sample.accel_mps2, sample.gyro_rads, dt_seconds, m_u_hat,
+                                  m_u_hat_valid);
+  sample.f_hat_unsigned = m_forwardAxisLearner.GetForwardAxisUnsigned();
+  sample.f_hat_locked = m_forwardAxisLearner.IsLocked();
 
   return sample;
 }
