39475a8fe85aad67ef9caa9e5bbb5074f4c9b697
Garrett Brown <garrett.brown@aclima.io>
2025-12-28 16:35:59 -0800

    Implement MPU6050 IMU processing

--- oasis_drivers_cpp/src/imu/Mpu6050ImuProcessor.h
+++ oasis_drivers_cpp/src/imu/Mpu6050ImuProcessor.h
@@ -8,16 +8,27 @@
 
 #pragma once
 
+#include <array>
+#include <cstdint>
+
 namespace OASIS::IMU
 {
 class Mpu6050ImuProcessor
 {
 public:
+  struct ProcessedSample
+  {
+    std::array<double, 3> accel_mps2{};
+    std::array<double, 3> gyro_rads{};
+  };
+
   Mpu6050ImuProcessor();
   ~Mpu6050ImuProcessor();
 
   void SetAccelScale(double accelScale);
   void SetGyroScale(double gyroScale);
+  ProcessedSample ProcessRaw(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy,
+                             int16_t gz) const;
 
 private:
   double m_accelScale = 0.0;
