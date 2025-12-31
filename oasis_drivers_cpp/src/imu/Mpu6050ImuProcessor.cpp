f2194ebad30ca12d4dda5f4a244e64e7c12c3ea2
Garrett Brown <eigendebugger@gmail.com>
2025-12-30 16:29:36 -0800

    drivers_cpp/py: Implement MPU6050 IMU driver

---
+++ oasis_drivers_cpp/src/imu/Mpu6050ImuProcessor.cpp
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) 2025 Garrett Brown
+ *  This file is part of OASIS - https://github.com/eigendude/OASIS
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  See the file LICENSE.txt for more information.
+ */
+
+#include "imu/Mpu6050ImuProcessor.h"
+
+using namespace OASIS::IMU;
+
+Mpu6050ImuProcessor::ProcessedSample Mpu6050ImuProcessor::ProcessRaw(
+    int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz)
+{
+  ProcessedSample sample{};
+
+  sample.accel_raw_mps2 = {static_cast<double>(ax) * m_accelScale,
+                           static_cast<double>(ay) * m_accelScale,
+                           static_cast<double>(az) * m_accelScale};
+  sample.gyro_rads = {static_cast<double>(gx) * m_gyroScale, static_cast<double>(gy) * m_gyroScale,
+                      static_cast<double>(gz) * m_gyroScale};
+
+  return sample;
+}
