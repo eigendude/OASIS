2e4e4c40ffb9357b7da47eb2f46c8ce0b8855763
Garrett Brown <garrett.brown@aclima.io>
2026-01-06 23:57:46 -0800

    control: Add EKF localizer

---
+++ oasis_control/oasis_control/localization/ekf/core/__init__.py
@@ -0,0 +1,20 @@
+################################################################################
+#
+#  Copyright (C) 2026 Garrett Brown
+#  This file is part of OASIS - https://github.com/eigendude/OASIS
+#
+#  SPDX-License-Identifier: Apache-2.0
+#  See DOCS/LICENSING.md for more information.
+#
+################################################################################
+
+"""
+Core EKF processing modules
+"""
+
+from __future__ import annotations
+
+from oasis_control.localization.ekf.core.ekf_core import EkfCore
+
+
+__all__ = ["EkfCore"]
