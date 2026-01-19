04fa971c1176e594900a37c5d1de85e5dae25dd1
Garrett Brown <garrett.brown@aclima.io>
2026-01-19 09:35:59 -0800

    Add AHRS bootstrap for gravity and magnetic field

--- oasis_control/oasis_control/localization/ahrs/ahrs_filter.py
+++ oasis_control/oasis_control/localization/ahrs/ahrs_filter.py
@@ -23,7 +23,11 @@ from oasis_control.localization.ahrs.ahrs_clock import AhrsClock
 from oasis_control.localization.ahrs.ahrs_config import AhrsConfig
 from oasis_control.localization.ahrs.ahrs_error_state import AhrsErrorStateLayout
 from oasis_control.localization.ahrs.ahrs_error_state import error_state_names
+from oasis_control.localization.ahrs.ahrs_linalg import is_finite_seq
 from oasis_control.localization.ahrs.ahrs_predict import predict_step
+from oasis_control.localization.ahrs.ahrs_quat import quat_conj_wxyz
+from oasis_control.localization.ahrs.ahrs_quat import quat_normalize_wxyz
+from oasis_control.localization.ahrs.ahrs_quat import quat_rotate_wxyz
 from oasis_control.localization.ahrs.ahrs_state import AhrsNominalState
 from oasis_control.localization.ahrs.ahrs_state import default_nominal_state
 from oasis_control.localization.ahrs.ahrs_timeline import AhrsTimeBuffer
@@ -40,10 +44,18 @@ from oasis_control.localization.ahrs.ahrs_types import AhrsSe3Transform
 from oasis_control.localization.ahrs.ahrs_types import AhrsStateData
 from oasis_control.localization.ahrs.ahrs_types import AhrsTime
 from oasis_control.localization.ahrs.ahrs_types import AhrsUpdateData
+from oasis_control.localization.ahrs.ahrs_types import ImuSample
 from oasis_control.localization.ahrs.ahrs_types import MagSample
 from oasis_control.localization.ahrs.ahrs_update_accel import update_accel
 from oasis_control.localization.ahrs.ahrs_update_gyro import update_gyro
 from oasis_control.localization.ahrs.ahrs_update_mag import update_mag
+
+
+# Dimensionless threshold for treating vectors as degenerate
+_EPS_NORM: float = 1.0e-6
+
+# Dimensionless threshold for detecting nearly opposite unit vectors
+_EPS_DOT: float = 1.0e-9
 
 
 class AhrsFilter:
@@ -320,6 +332,7 @@ class AhrsFilter:
     def _apply_event(self, event: AhrsEvent) -> None:
         if event.event_type == AhrsEventType.IMU:
             payload_imu: AhrsImuPacket = cast(AhrsImuPacket, event.payload)
+            self._maybe_initialize_gravity(payload_imu.imu)
             gyro_report: AhrsUpdateData
             if self._x is None or self._p is None:
                 gyro_report = self._build_update(
@@ -363,6 +376,7 @@ class AhrsFilter:
 
         if event.event_type == AhrsEventType.MAG:
             payload_mag: MagSample = cast(MagSample, event.payload)
+            self._maybe_initialize_magnetic_field(payload_mag)
             mag_report: AhrsUpdateData
             if self._x is None or self._p is None:
                 mag_report = self._build_update(
@@ -663,3 +677,113 @@ class AhrsFilter:
             )
 
         self._t_state = t_target
+
+    def _maybe_initialize_gravity(self, imu: ImuSample) -> None:
+        if self._x is None:
+            return
+
+        if _norm3(self._x.g_w_mps2) >= _EPS_NORM:
+            return
+
+        accel: list[float] = list(imu.linear_acceleration_mps2)
+        if len(accel) != 3 or not is_finite_seq(accel):
+            return
+
+        accel_norm: float = _norm3(accel)
+        if accel_norm < _EPS_NORM:
+            return
+
+        # m/s^2 gravity magnitude derived from stationary accel norm
+        g_mag: float = accel_norm
+        self._x.g_w_mps2 = [0.0, 0.0, -g_mag]
+
+        accel_unit: list[float] = _scale3(accel, 1.0 / accel_norm)
+        gravity_unit: list[float] = [0.0, 0.0, -1.0]
+        self._x.q_wb_wxyz = _quat_from_two_unit_vectors(gravity_unit, accel_unit)
+
+    def _maybe_initialize_magnetic_field(self, mag: MagSample) -> None:
+        if self._x is None:
+            return
+
+        if _norm3(self._x.m_w_t) >= _EPS_NORM:
+            return
+
+        if _norm3(self._x.g_w_mps2) < _EPS_NORM:
+            return
+
+        magnetic: list[float] = list(mag.magnetic_field_t)
+        if len(magnetic) != 3 or not is_finite_seq(magnetic):
+            return
+
+        magnetic_norm: float = _norm3(magnetic)
+        if magnetic_norm < _EPS_NORM:
+            return
+
+        q_wb: list[float] = list(self._x.q_wb_wxyz)
+        if len(q_wb) != 4 or not is_finite_seq(q_wb):
+            return
+
+        q_bw: list[float] = quat_conj_wxyz(q_wb)
+        magnetic_world: list[float] = quat_rotate_wxyz(q_bw, magnetic)
+        if not is_finite_seq(magnetic_world):
+            return
+
+        self._x.m_w_t = magnetic_world
+
+
+def _norm3(v: list[float]) -> float:
+    vx: float = v[0]
+    vy: float = v[1]
+    vz: float = v[2]
+    return math.sqrt(vx * vx + vy * vy + vz * vz)
+
+
+def _dot3(a: list[float], b: list[float]) -> float:
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+
+
+def _cross3(a: list[float], b: list[float]) -> list[float]:
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0],
+    ]
+
+
+def _scale3(v: list[float], scale: float) -> list[float]:
+    return [value * scale for value in v]
+
+
+def _unit3(v: list[float]) -> tuple[list[float], bool]:
+    norm: float = _norm3(v)
+    if norm < _EPS_NORM:
+        return [0.0, 0.0, 0.0], False
+    return _scale3(v, 1.0 / norm), True
+
+
+def _orthogonal_unit(v: list[float]) -> list[float]:
+    axis: list[float]
+    if abs(v[0]) < 0.9:
+        axis = [1.0, 0.0, 0.0]
+    else:
+        axis = [0.0, 1.0, 0.0]
+    orthogonal: list[float] = _cross3(v, axis)
+    orthogonal_unit: list[float]
+    orthogonal_unit, valid = _unit3(orthogonal)
+    if not valid:
+        return [0.0, 0.0, 1.0]
+    return orthogonal_unit
+
+
+def _quat_from_two_unit_vectors(
+    from_unit: list[float], to_unit: list[float]
+) -> list[float]:
+    dot: float = _dot3(from_unit, to_unit)
+    if dot > 1.0 - _EPS_DOT:
+        return [1.0, 0.0, 0.0, 0.0]
+    if dot < -1.0 + _EPS_DOT:
+        axis: list[float] = _orthogonal_unit(from_unit)
+        return [0.0, axis[0], axis[1], axis[2]]
+    cross: list[float] = _cross3(from_unit, to_unit)
+    quat: list[float] = [1.0 + dot, cross[0], cross[1], cross[2]]
+    return quat_normalize_wxyz(quat)
